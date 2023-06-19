18562ad9da2278e69b9fe857db900481b8b4577d
Mark Anthony <manthonyamaro@yahoo.com>
2023-06-19 18:55:30 -0500

    created javascript that prompts the user a series of questions based on what iprovided and store their responses. five saparate questions were made to adhere to the criteria using the inquirer package

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'location',
+            message: 'Where do you live?',
+
+        },
+        {
+            type: 'input',
+            name: 'Bio',
+            message: 'What is your info?',
+        },
+        {
+            type: 'input',
+            name: 'linkedin',
+            message: "What is your linkedin URL",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'whats your Hub'
+
+        },
+    ])
